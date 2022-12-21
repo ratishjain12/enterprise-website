8d1826c3e02f15975858c7102e6e10ebade88fe3
ratish jain <ratishjain@ratishs-MacBook-Air.local>
2022-12-21 20:04:39 +0530

    responsiveness

--- src/App.js
+++ src/App.js
@@ -194,7 +194,7 @@ function App() {
             </Grid>
           </center>
         </div>
-        <div style={{ marginBottom: "12%" }}>
+        <div style={{ marginBottom: "112px" }}>
           <center>
             <Grid container spacing={2}>
               <Grid item xs={12} lg={6}>
